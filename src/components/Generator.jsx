ad27a323429d064d721e897d4e5d7056781ccb61
tushar6707-Aenaen <chauhantushar6706@gmail.com>
2025-09-15 21:17:41 +0530

    Fix: Update repository structure and configurations

---
+++ src/components/Generator.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Generator = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Generator
---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className='min-h-screen text-center flex flex-col gap-10 items-center justify-center max-w-[800px] w-full mx-auto'>
+            <div className='flex flex-col gap-4'>
+
+            <p>It's time to get</p>
+            <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>Swole<span className='text-blue-400'>NORMOUS</span></h1>
+            </div>
+            <p className='text-sm md:text-base font-light'>I hereby acknowledgement that I may become <span className='text-blue-400 font-medium'>unbelievably swolenormous</span> and accept all risks of becoming the local <span className='text-blue-400 font-medium'> mass montrosity</span>, afflicted with severe body dismorphia, unable to fit through doors.</p>
+            <button className='px-8 py-4 rounded-md border border-blue-400 border-solid bg-slate-950 blueShadow duration-200'><p>Accept and Begin</p></button>
+        </div>
+    )
+}
+
+export default Hero
